6fa4b3bc787d9972749ce5fca18221c4018bd5bd
Jatin Shakya <jatin1306888@gmail.com>
2025-08-27 08:54:36 +0000

    jaha ads dikhegi baha kuch likho jab tak ads nahi dikhti

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -7,8 +7,14 @@ const AdBanner = () => {
 
   useEffect(() => {
     if (adContainerRef.current) {
-        adContainerRef.current.innerHTML = `
-          <script type="text/javascript">
+        // Clear the placeholder text before injecting the ad script
+        adContainerRef.current.innerHTML = '';
+        
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        
+        // Using innerHTML for the options script
+        script.innerHTML = `
             atOptions = {
               'key' : '7dbc62c8ffa684dad3b4c20f4bb0654d',
               'format' : 'iframe',
@@ -16,13 +22,26 @@ const AdBanner = () => {
               'width' : 320,
               'params' : {}
             };
-          </script>
-          <script type="text/javascript" src="//www.highperformanceformat.com/7dbc62c8ffa684dad3b4c20f4bb0654d/invoke.js"></script>
         `;
+        adContainerRef.current.appendChild(script);
+
+        const invokeScript = document.createElement('script');
+        invokeScript.type = 'text/javascript';
+        invokeScript.src = '//www.highperformanceformat.com/7dbc62c8ffa684dad3b4c20f4bb0654d/invoke.js';
+        
+        adContainerRef.current.appendChild(invokeScript);
     }
   }, []);
 
-  return <div ref={adContainerRef} className="ad-banner-container flex justify-center items-center my-4" style={{ minHeight: '50px' }}></div>;
+  return (
+    <div 
+      ref={adContainerRef} 
+      className="ad-banner-container flex justify-center items-center my-4 bg-gray-100 dark:bg-gray-800 text-gray-500 rounded-md" 
+      style={{ minHeight: '50px', width: '320px', margin: '1rem auto' }}
+    >
+      विज्ञापन लोड हो रहा है...
+    </div>
+  );
 };
 
 export default AdBanner;
