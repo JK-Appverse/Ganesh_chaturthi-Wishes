32fba31a0784173a9d46e8a3d874df928bcbde3d
Jatin Shakya <jatin1306888@gmail.com>
2025-08-27 08:53:33 +0000

    bhai ads nahi dikh rahi h

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -6,24 +6,19 @@ const AdBanner = () => {
   const adContainerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (adContainerRef.current && adContainerRef.current.children.length === 0) {
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.innerHTML = `
-        atOptions = {
-          'key' : '7dbc62c8ffa684dad3b4c20f4bb0654d',
-          'format' : 'iframe',
-          'height' : 50,
-          'width' : 320,
-          'params' : {}
-        };
-      `;
-      adContainerRef.current.appendChild(script);
-
-      const invokeScript = document.createElement('script');
-      invokeScript.type = 'text/javascript';
-      invokeScript.src = '//www.highperformanceformat.com/7dbc62c8ffa684dad3b4c20f4bb0654d/invoke.js';
-      adContainerRef.current.appendChild(invokeScript);
+    if (adContainerRef.current) {
+        adContainerRef.current.innerHTML = `
+          <script type="text/javascript">
+            atOptions = {
+              'key' : '7dbc62c8ffa684dad3b4c20f4bb0654d',
+              'format' : 'iframe',
+              'height' : 50,
+              'width' : 320,
+              'params' : {}
+            };
+          </script>
+          <script type="text/javascript" src="//www.highperformanceformat.com/7dbc62c8ffa684dad3b4c20f4bb0654d/invoke.js"></script>
+        `;
     }
   }, []);
 
