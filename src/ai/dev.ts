1188f9343ee76e7591319aabfcb4a949c88ad67c
Jatin Shakya <jatin1306888@gmail.com>
2025-10-19 09:50:11 +0000

    Try fixing this error: `Build Error: Export genkitNext doesn't exist in

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import {genkitEval, GenkitMetric} from '@genkit-ai/evaluator';
-// import {googleCloud} from '@genkit-ai/google-cloud';
 
 import * as datastore from './datastore';
 import * as flows from './flows';
@@ -9,7 +8,6 @@ import * as flows from './flows';
 export default genkit({
   plugins: [
     googleAI(),
-    // googleCloud(),
     genkitEval({
       judge: 'googleai/gemini-1.5-flash',
       metrics: [GenkitMetric.FAITHFULNESS, GenkitMetric.MALICIOUSNESS],
--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {genkitNext} from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [genkitNext(), googleAI()],
+  plugins: [googleAI()],
   logLevel: 'debug',
   enableTracing: true,
 });
